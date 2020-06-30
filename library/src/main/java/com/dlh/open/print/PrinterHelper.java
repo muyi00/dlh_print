f087512374be6ee146bca72a7b2b630a3413c597
muyi000 <2672569939@qq.com>
2020-06-30 14:29:28 +0800

    1.0.2优化

--- library/src/main/java/com/dlh/open/print/PrinterHelper.java
+++ library/src/main/java/com/dlh/open/print/PrinterHelper.java
@@ -295,6 +295,10 @@ public class PrinterHelper implements GenericLifecycleObserver {
             //蓝牙没有开启，开启蓝牙
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+        } else {
+            if (bluetoothCallback != null) {
+                bluetoothCallback.bluetoothEnabled(mContext.getString(R.string.app_bluetooth_is_turned_on));
+            }
         }
     }
 
